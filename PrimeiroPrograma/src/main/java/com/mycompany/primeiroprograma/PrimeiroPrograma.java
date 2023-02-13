a0ff211dfd2867140f93952b9c4f3423fc930248
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-13 12:41:40 -0200

    Create PrimeiroPrograma.java

---
+++ PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package com.mycompany.primeiroprograma;
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+        System.out.println("Me livrei da maldição");
+        System.out.println("sout + tab");
+        System.out.println("print = escreve algo");
+        System.out.println("println = esreve algo e pula pra linha de baixo");
+        System.out.println("psvm + tab = monsta o public static void main(String[] args{");
+        System.out.print("Linha 1");
+        System.out.println(" Linha 2");
+        System.out.println("Sempre que a 1° letra é Maiúscula é Classe");
+        System.out.println("Exemplo : MinhaClasse AlunoCursoEmVideo");
+        System.out.println("Se 1°for Minúscula ela pode ser : Atributo, Variável ou Método");
+        System.out.println("Exemplo : nomeAluno mediaPrimeiroBimestre lancarNotaAluno");
+        System.out.println("Se for tudo em Minúsculo é pacote");
+        System.out.println("Exemplo : meu_pacote alunocursoemvideo");
+        System.out.println("Se for tudo em Maiúsuclo é constante");
+        System.out.println("Exemplo MINHA_CONSTANTE VALOR_DE_PI");
+        System.out.println("");
+        System.out.println("Exercícios : ");
+        System.out.println("Casa, casinha, casaNova, CasaVelha, CASARÃO");
+        System.out.println("Classe = 1° Letra é Maiúscula, temos : Casa, CasaVelha");
+        System.out.println(" , casinha, casaNova, , CASARÃO");
+    }
+}
